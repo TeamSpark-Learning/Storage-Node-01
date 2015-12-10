0a663a87f7c28e30c41d2b0e5b3ebe62ac6453d3
Anton Boyko <boyko.ant@live.com>
2015-12-10 15:58:37 +0200

    add select entities by custom property script

---
+++ TableStorage/07-query-filter.js
@@ -0,0 +1,47 @@
+var config = require('./config');
+var colors = require('colors');
+
+var azureStorage = require('azure-storage');
+var azureTableStorage = azureStorage.createTableService(config.accountName, config.accountKey);
+
+var timeStart = new Date();
+var timeFinish = null;
+var recordsSelected = 0;
+var queriesCount = 0;
+
+var category = 'critical';
+
+function getByCustomProperty() {
+	var filter = azureStorage.TableQuery.stringFilter('LogCategory', azureStorage.TableUtilities.QueryComparisons.EQUAL, category);
+	var query = new azureStorage.TableQuery().where(filter);
+	azureTableStorage.queryEntities(config.tableNameLog, query, null, queryEntitiesCallback);
+}
+
+function queryEntitiesCallback(error, result) {
+	if (!error) {
+		recordsSelected += result.entries.length;
+		queriesCount++;
+		
+		if (!!result.continuationToken) {
+			var filter = azureStorage.TableQuery.stringFilter('LogCategory', azureStorage.TableUtilities.QueryComparisons.EQUAL, category);
+			var query = new azureStorage.TableQuery().where(filter);
+			azureTableStorage.queryEntities(config.tableNameLog, query, result.continuationToken, queryEntitiesCallback);
+		} else {
+			timeFinish = new Date();
+		
+			var diff = timeFinish.getTime() - timeStart.getTime();
+			var avg = diff / recordsSelected;
+			
+			console.info('time start:        ' + timeStart.toISOString().red);
+			console.info('time finish:       ' + timeFinish.toISOString().red);
+			console.info('records selected:  ' + recordsSelected.toString().red);
+			console.info('queries count:     ' + queriesCount.toString().red);
+			console.info('total time in ms:  ' + diff.toString().red);
+			console.info('avg ms per record: ' + avg.toString().red);
+		}
+	} else {
+		console.error(error.toString().red);
+	}
+}
+
+getByCustomProperty();
