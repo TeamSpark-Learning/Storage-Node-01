5ac68dd58b1b8e062ebac11997e377ea4acfc604
Anton Boyko <boyko.ant@live.com>
2015-12-11 17:37:31 +0200

    cleanup config

--- TableStorage/config.js
+++ TableStorage/config.js
@@ -1,6 +1,6 @@
 var config = {
-	accountName: 'aboykotraining',
-	accountKey: 'X8XCGpQjD3vTNmUTHmhfeKaXg7IJFgSa1aMSmzanZvvkNJvvi0OrymbZ3Te6LQb4pHZ/fHx0zA1spkuzkbh+xg==',
+	accountName: '',
+	accountKey: '',
 	tableNameLog: 'logs'
 };
 
